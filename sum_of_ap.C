8066f0b3f9cbe3681222fe1583189375734000e3
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-24 16:14:46 +0530

    sum_of_ap.C

---
+++ sum_of_ap.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,d,n,i,tn;
+int sum=0;
+printf("enter the first term:",a);
+scanf("%d",&a);
+printf("enter the difference between each terms:",d);
+scanf("%d",&d);
+printf("enter the last term:",n);
+scanf("%d",&n);
+sum=(n*(2*a+(n-1)*d))/2;
+tn=a+(n-1)*d;
+printf("the sum of AP series is:");
+for(i=a;i<=tn;i=i+d)
+{
+if(i!=tn)
+printf("%d+",i);
+else
+printf("%d=%d",i,sum);
+}
+return 0;
+}
